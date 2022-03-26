df54a0acd889d52c61146a1d77a45d238c699b60
cwiecha <308743+cwiecha@users.noreply.github.com>
2022-03-26 09:48:00 +0200

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -1,46 +1,37 @@
 var AWS = require('aws-sdk');
 AWS.config.update({region: 'us-west-2'});
-var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-const http = require('http');
-
-function sqsWrite(params, callbk) {
-    console.log('In sqsWrite, params: ' + params );
-    sqs.sendMessage(params, function(err, data) {
-    if (err) {
-        console.log("Error", err);
-    } else {
-        console.log("Success", data.MessageId);
-    }
-});
+async function createItem(params){
+  try {
+    await docClient.put(params).promise();
+  } catch (err) {
+    return err;
+  }
 }
 
 exports.handler = (event, context, callback) => {
 
-    event.Records.forEach((record) => {
+    event.Records.forEach( (record) => {
         var record_str = JSON.stringify(record, null, 2);
         console.log('Stream record: ', record_str);
-        console.log('incoming ')
 
         if (record.eventName == 'INSERT') {
-            // callAPI("rows/all", result => console.log(result))
 
             var params = {
-             // Remove DelaySeconds parameter and value for FIFO queues
-                MessageAttributes: {
-                    "MyLabel": {
-                     DataType: "String",
-                     StringValue: "Test attribute"
-                    }
-                },
-                MessageBody: record.dynamodb.NewImage.msg.S,
-                QueueUrl: "<your queue url>"
-            };
-
-            sqsWrite(params, result => console.log(result));
+                TableName : 'Lab4Logs',
+                Item: {
+                    "partition_key" : record.dynamodb.NewImage.partition_key.S,
+                    "sort_key" : record.dynamodb.NewImage.sort_key.S,
+                    "Class": record.dynamodb.NewImage.Class.S,
+                    "Confidence": record.dynamodb.NewImage.Confidence.S,
+                    "Label": record.dynamodb.NewImage.Label.S,
+                    "Features": record.dynamodb.NewImage.Features.S
+                }
+            }
+            createItem(params)
         }
-    });
-
-
+    }),
+    
     callback(null, `Successfully processed ${event.Records.length} records.`);
-};
+}
