ca8575246cd3273beb7dbcf4bfb8f462ebff273b
cwiecha <308743+cwiecha@users.noreply.github.com>
2022-03-26 09:45:32 +0200

    Update DynamoDB_writer_example.py

--- DynamoDB_writer_example.py
+++ DynamoDB_writer_example.py
@@ -17,12 +17,18 @@ table = dynamodb.Table('<your dynamodb table name')
 from boto3.dynamodb.conditions import Key, Attr
 
 
-def post_score():
-    response = table.put_item(
+def post_score(log_table, feature_string, class_string, conf_string, label):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    response = log_table.put_item(
        Item={
-            'partition_key': "from writer",
+            'partition_key': current_time,
             'sort_key': "abc",
-            'Message': "test message not yet json"
+            'Features': feature_string,
+            'Class' : class_string,
+            'Confidence' : conf_string,
+            'Label': label
             }
     )
-    return response
+
